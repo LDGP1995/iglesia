34acdf76bbb7b3b7e6612f7e20f4a0a69d9e882e
luiso <luisdgp1995@gmail.com>
2024-05-23 01:04:35 +0200

    header2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import './App.css'
-import Navbar from './componentes/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Contactenos from './componentes/Navbar/pages/Contactenos'
-import Eventos from './componentes/Navbar/pages/Eventos'
-import Nosotros from './componentes/Navbar/pages/Nosotros'
-import Servicios from './componentes/Navbar/pages/Servicios'
+import "./App.css";
+import Navbar from "./componentes/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Contactenos from "./componentes/Navbar/pages/Contactenos"
+import Eventos from "./componentes/Navbar/pages/Eventos";
+import Nosotros from "./componentes/Navbar/pages/Nosotros"
+import Servicios from "./componentes/Navbar/pages/Servicios";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Routes>
-          <Route path="/contactenos" element={<Contactenos/>} />
-          <Route path="/eventos" element={<Eventos/>} />
-          <Route path="/nosotros" element={<Nosotros/>} />
-          <Route path="/servicios" element={<Servicios/>} />
-      </Routes>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/contactenos" element={<Contactenos />} />
+          <Route path="/eventos" element={<Eventos />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/servicios" element={<Servicios />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,27 +1,47 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import Hamburger from "./menu.png";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <nav className="navbar">
-      <Link to="/" className="logo">
-        Familia Cristiana
-      </Link>
-      <ul>
-        <li>
-          <Link to="/eventos">Eventos</Link>
-        </li>
-        <li>
-          <Link to="/contactenos">Contactenos</Link>
-        </li>
-        <li>
-          <Link to="/servicios">Servicios</Link>
-        </li>
-        <li>
-          <Link to="/nosotros">Nosotros</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Familia Cristiana
+        </Link>
+
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <Link to="/eventos" className="nav-menu-link">
+              Eventos
+            </Link>
+          </li>
+          <li>
+            <Link to="/contactenos" className="nav-menu-link">
+              Contactenos
+            </Link>
+          </li>
+          <li>
+            <Link to="/servicios" className="nav-menu-link">
+              Servicios
+            </Link>
+          </li>
+          <li>
+            <Link to="/nosotros" className="nav-menu-link">
+              Nosotros
+            </Link>
+          </li>
+        </ul>
+
+        <button className="button" onClick={()=>{
+          setMenuOpen(!menuOpen)
+        }}>
+          <img src={Hamburger}></img>
+        </button>
+      </nav>
+    </div>
   );
 };
 
