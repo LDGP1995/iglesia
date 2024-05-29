dbbf4f3a764bc831c0776a263455a76c013d3f9d
luiso <luisdgp1995@gmail.com>
2024-05-29 19:05:29 +0200

    header2

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -5,6 +5,10 @@ import React, { useState } from "react";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("/");
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
   return (
     <div>
       <nav className="navbar">
@@ -14,30 +18,46 @@ const Navbar = () => {
 
         <ul className={menuOpen ? "open" : ""}>
           <li>
-            <Link to="/eventos" className="nav-menu-link">
+            <Link
+              to="/"
+              className={activeLink === "/" ? "active-link" : "nav-link"}
+              onClick={() => handleLinkClick("/")}
+            >
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link to="/eventos" className={activeLink === "/eventos" ? "active-link" : "nav-link"}
+              onClick={() => handleLinkClick("/eventos")}>
               Eventos
             </Link>
           </li>
           <li>
-            <Link to="/contactenos" className="nav-menu-link">
+            <Link to="/contactenos" className={activeLink === "/contactenos" ? "active-link" : "nav-link"}
+              onClick={() => handleLinkClick("/contactenos")}>
               Contactenos
             </Link>
           </li>
           <li>
-            <Link to="/servicios" className="nav-menu-link">
+            <Link to="/servicios" className={activeLink === "/servicios" ? "active-link" : "nav-link"}
+              onClick={() => handleLinkClick("/servicios")}>
               Servicios
             </Link>
           </li>
           <li>
-            <Link to="/nosotros" className="nav-menu-link">
+            <Link to="/nosotros" className={activeLink === "/nosotros" ? "active-link" : "nav-link"}
+              onClick={() => handleLinkClick("/nosotros")}>
               Nosotros
             </Link>
           </li>
         </ul>
 
-        <button className="button" onClick={()=>{
-          setMenuOpen(!menuOpen)
-        }}>
+        <button
+          className="button"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
           <img src={Hamburger}></img>
         </button>
       </nav>
